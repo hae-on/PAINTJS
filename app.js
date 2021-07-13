988cffb5524eac13abe86c945a2169d22b640b02
hae-on <solbi2004@naver.com>
2021-07-13 22:40:43 +0900

    [add]colors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
 
+// 브러쉬 색상 기본값
 ctx.strokeStyle = "#2c2c2c";
+// 브러쉬 굵기 기본값
 ctx.lineWidth = 2.5;
 
 let painting = false;
@@ -29,9 +32,20 @@ function onMouseMove(event) {
   }
 }
 
+function handleColorClick(event) {
+  // 각 색상의 rgb 값 추출
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
+
+// 색상표 하나하나를 눌렀을 때 배열 형태로 함수 도출
+Array.from(colors).forEach((color) =>
+  color.addEventListener("click", handleColorClick)
+);
